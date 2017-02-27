3c48453d8a80830776564053cc65efb1e6c27ca3
Marcos Barbosa <barbosa.mg@gmail.com>
2017-02-27 00:42:39 -0300

    Listando elementos da tabela a partir de um arquivo json

--- js/app.js
+++ js/app.js
@@ -1,51 +1,19 @@
-var hello = new Vue({
-	el: '#hello',
+var app = new Vue({
+	el: '#app',
 	data: {
-		msg: "Hello Vue!",
-		peoples: [
-			{name: "Maria"},
-			{name: "Pedro"},
-			{name: "Gustavo"},
-			{name: "Ana"},
-			{name: "Gabriela"}
-		],
-		newElement: '',
-		elements: [],
-		objectA: {
-			'font-size': '30px'
-		},
-		objectB: {
-			color: 'red'
-		},
-		myListForm: [],
-		myForm: {
-			name: '',
-			email: ''
-		}
+		books: []
 	},
 	methods: {
-		addElement: function() {
-			var title = this.newElement.trim();
+	},
+	ready: function() {
 
-			if(title) {
-				this.elements.push({title: title});
-				this.newElement = "";
-			}
-		},
-		removeElement: function(e, index) {
-			e.preventDefault();
-			this.elements.splice(index, 1);
-		},
-		myClick: function() {
-			alert('click')
-		},
-		myKeyUp: function() {
-			alert('myKeyUp')
-		},
-		addForm: function() {
-			this.myListForm.push({name: this.myForm.name, email: this.myForm.email});
-			this.myForm.name = '';
-			this.myForm.email = '';
-		}
+		var self = this;
+
+		self.$http.get('dataServer.json').then(function(response) {
+			// console.log(response);
+
+			self.books = response.data;
+		});
+
 	}
 });

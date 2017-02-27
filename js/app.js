262457d7a2f4f0d6eea8b6fd1af21f1ad93a3448
Marcos Barbosa <barbosa.mg@gmail.com>
2017-02-27 01:30:59 -0300

    Criando ordenação, acrescentando ícones para asc e desc na coluna que está sendo ordenada

--- js/app.js
+++ js/app.js
@@ -2,9 +2,20 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		books: [],
-		mySearch: ''
+		mySearch: '',
+		orderCol: 'id',
+		orderInverse: -1
 	},
 	methods: {
+		filterOrderBy: function(e, col) {
+
+			e.preventDefault();
+
+			this.orderCol = col;
+
+			this.orderInverse = this.orderInverse * -1;
+
+		}
 	},
 	ready: function() {
 

5a7ce50a2e990266e51f429b45115b53d4a56d71
Marcos Barbosa <barbosa.mg@gmail.com>
2017-02-27 00:22:20 -0300

    Implementando os primeiros exemplos

--- js/app.js
+++ js/app.js
@@ -8,6 +8,44 @@ var hello = new Vue({
 			{name: "Gustavo"},
 			{name: "Ana"},
 			{name: "Gabriela"}
-		]
+		],
+		newElement: '',
+		elements: [],
+		objectA: {
+			'font-size': '30px'
+		},
+		objectB: {
+			color: 'red'
+		},
+		myListForm: [],
+		myForm: {
+			name: '',
+			email: ''
+		}
+	},
+	methods: {
+		addElement: function() {
+			var title = this.newElement.trim();
+
+			if(title) {
+				this.elements.push({title: title});
+				this.newElement = "";
+			}
+		},
+		removeElement: function(e, index) {
+			e.preventDefault();
+			this.elements.splice(index, 1);
+		},
+		myClick: function() {
+			alert('click')
+		},
+		myKeyUp: function() {
+			alert('myKeyUp')
+		},
+		addForm: function() {
+			this.myListForm.push({name: this.myForm.name, email: this.myForm.email});
+			this.myForm.name = '';
+			this.myForm.email = '';
+		}
 	}
 });
